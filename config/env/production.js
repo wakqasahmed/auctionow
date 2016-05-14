652958aef89a0bc332f0cd302ccd28af2d874513
Waqas Ahmed <wakqasahmed@gmail.com>
2016-05-14 23:29:48 +0400

    heroku redis hardcoded credentials

--- config/env/production.js
+++ config/env/production.js
@@ -19,10 +19,10 @@ module.exports = {
     sync: process.env.DB_SYNC || false //Synchronizing any model changes with database
   },
   redis: {
-    host: process.env.REDIS_HOST || process.env.OPENSHIFT_REDIS_HOST || "localhost",
-    port: process.env.REDIS_PORT || process.env.OPENSHIFT_REDIS_PORT || 6379,
+    host: process.env.REDIS_HOST || process.env.OPENSHIFT_REDIS_HOST || "ec2-54-83-207-91.compute-1.amazonaws.com" || "localhost",
+    port: process.env.REDIS_PORT || process.env.OPENSHIFT_REDIS_PORT || 18059 || 6379,
     database: parseInt(process.env.REDIS_DATABASE) || 0,
-    password: process.env.REDIS_PASSWORD || "",
+    password: process.env.REDIS_PASSWORD || "pdhtkg8fniaep87u0p5e1uenvoj" || "",
   },
   log: {
     // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
